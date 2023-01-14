40b8ca6c188daa3fa56442e995e684445996af50
Nicholas Csaszar <ncsaszar97@gmail.com>
2023-01-14 17:11:55 -0500

    basic nav panel

--- web-client/src/pages/Navbar.tsx
+++ web-client/src/pages/Navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ menuOpen, setMenuOpen }) => {
   console.log(menuOpen);
   return (
-    <nav className="bg-gray-800 p-4 mt-0 fixed w-full z-10 top-0">
+    <nav className="fixed top-0 z-10 bg-gray-800 p-4 mt-0 w-full">
       <div className="container mx-auto flex flex-wrap items-center">
         <div className="flex space-x-4 w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
           <button onClick={() => setMenuOpen(!menuOpen)}>
