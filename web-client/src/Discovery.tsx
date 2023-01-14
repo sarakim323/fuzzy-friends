1d1a878f7b6ac9ae672f76231e6b7d8825a49552
Brandon Vega <vegabrandon1250@gmail.com>
2023-01-14 12:24:40 -0500

    Added comments for skeleton divs.

--- web-client/src/Discovery.tsx
+++ web-client/src/Discovery.tsx
@@ -3,7 +3,25 @@ import React from 'react';
 const Discovery = () => {
   return (
     <div>
-      <div className="flex flex-row">Example navbar</div>
+      {/* Navbar example */}
+      <div className="flex flex-row"></div>
+
+      {/* Profile info */}
+      <div>
+        <div>First Name</div>
+        <div>Location</div>
+      </div>
+
+      {/* Carousel */}
+      <div>
+        {/* Images */}
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Yes/No buttons */}
+      <div></div>
+      <div></div>
     </div>
   );
 };
