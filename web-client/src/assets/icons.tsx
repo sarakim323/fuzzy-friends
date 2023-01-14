d3c68ce4cd9db098027b77db7ace61c7d09752f0
Nicholas Csaszar <ncsaszar97@gmail.com>
2023-01-14 14:04:58 -0500

    Sidebar

---
+++ web-client/src/assets/icons.tsx
@@ -0,0 +1,13 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import MailIcon from '@mui/icons-material/Mail';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+export const categories = [
+  { name: 'Profile', icon: <AccountCircleIcon /> },
+  { name: 'Discover', icon: <TravelExploreIcon /> },
+  { name: 'Message', icon: <MailIcon /> },
+  { name: 'Events', icon: <CalendarMonthIcon /> },
+  { name: 'Logout', icon: <LogoutIcon /> },
+];
