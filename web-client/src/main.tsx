39d498c26bd659950df320fd3e4373d0b6c81a79
Joon Hwang <joonhwangs@gmail.com>
2023-01-17 15:45:30 -0500

    message

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import Auth0ProviderWithNavigate from './auth0-provider';
 import './index.css';
 import { Router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <Router />
+    <Auth0ProviderWithNavigate>
+      <Router />
+    </Auth0ProviderWithNavigate>
   </BrowserRouter>
 );
