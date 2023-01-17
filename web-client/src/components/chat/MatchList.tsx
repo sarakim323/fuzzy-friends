4ef2df2090e54e170c55b1bba9c5e9fc19a8a30e
Joon Hwang <joonhwangs@gmail.com>
2023-01-17 13:34:16 -0500

    started to add on to match list

--- web-client/src/components/chat/MatchList.tsx
+++ web-client/src/components/chat/MatchList.tsx
@@ -1,8 +1,20 @@
-import { useState } from 'react';
-import { Navbar } from '../';
+import React, { useEffect, useState } from 'react';
 
-function MatchList() {
-  return <div>MatchList </div>;
+function MatchList({ matchedUsers }) {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+  const [currentUserSelected, setCurrentUserSelected] = useState(undefined);
+
+  // useEffect(() => {}, []);
+
+  return (
+    <div className="flex bg-warmGray-200">
+      <div>
+        <h3>snappy</h3>
+      </div>
+      <div className="matchList"></div>
+    </div>
+  );
 }
 
 export default MatchList;
--- web-client/src/components/chat/ProfileBox.tsx
+++ web-client/src/components/chat/ProfileBox.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Navbar } from '../';
+// import { Navbar } from '../';
 
 function ProfileBox() {
   return <div>ProfileBox</div>;
