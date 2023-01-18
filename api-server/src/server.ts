7fa98d4d6b8344e63506223621a1cc4c91c3657b
nathan7432 <steelersfan5052@yahoo.com>
2023-01-18 09:45:20 -0500

    edit mock

--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 const app = express();
 const port = 3000;
-import { discover, friends } from './mock_data';
+import { discover, friends, messages } from './mock_data';
 import { Request, Response } from 'express';
 
 app.use(express.json());
@@ -12,12 +12,20 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+app.post('/users/:id/messages/', (req: Request, res: Response) => {
+  res.sendStatus(200)
+})
+
+app.get('/user/:id/messages/:friendId', (req: Request, res: Response) => {
+  res.send(messages)
+})
+
 app.get('/users/:id/friends', (req: Request, res: Response) =>
   res.send(friends)
 );
 
 app.put('/users/:id/friends/*', (req: Request, res: Response) => {
-  res.send(200);
+  res.sendStatus(200);
 });
 
 app.get('/users/:id/discover', (req: Request, res: Response) => {
