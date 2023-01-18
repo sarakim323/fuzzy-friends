19da527899bda624eb90a09c5d93475579305fba
Joon Hwang <joonhwangs@gmail.com>
2023-01-17 19:56:21 -0500

    commit without sara change

--- web-client/src/components/chat/ChatBox.tsx
+++ web-client/src/components/chat/ChatBox.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState, useRef } from 'react';
 // import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
 import { sendMessageRoute, receiveMessageRoute } from '../utils/APIRoutes';
 import ChatInput from './ChatBox';
 
-function ChatBox({ currentChat, socket }) {
+function ChatBox({ currentChat }) {
   const [messages, setMessages] = useState([]);
   const scrollRef = useRef();
   const [arrivalMessage, setArrivalMessage] = useState(null);
@@ -14,20 +13,12 @@ function ChatBox({ currentChat, socket }) {
   };
 
   useEffect(() => {
-    if (socket.current) {
-      socket.current.on('msg-receive', (msg) => {
-        setArrivalMessage({ fromSelf: false, message: msg });
-      });
-    }
-  }, []);
-
-  useEffect(() => {
     arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
   }, [arrivalMessage]);
 
-  useEffect(() => {
-    scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messages]);
+  // useEffect(() => {
+  //   scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
+  // }, [messages]);
 
   return (
     <div>
--- web-client/src/components/chat/ProfileBox.tsx
+++ web-client/src/components/chat/ProfileBox.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 // import { Navbar } from '../';
 
-function ProfileBox() {
+function ProfileBox({ matches }) {
+  console.log(matches[0].profile_pic, '12');
+  const pic = matches[0].profile_pic;
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+  const [currentUserSelected, setCurrentUserSelected] = useState(undefined);
+
   return (
-    <div className="flex bg-warmGray-200">
+    <div>
       <div className="m-10 p-20 max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-warmGray-700 dark:border-gray- h-110 gap-2 content-center">
-        <div className="relative w-48 h-48 ml-16">
+        <div className="flex items-center justify-between">
           <img
-            className="absolute rounded-full border border-gray-300 shadow=sm"
-            // src={pic}
+            className="mt-6 w-fit rounded-full border border-gray-300 shadow=sm"
+            src={pic}
             alt="profile photo"
           />
         </div>
