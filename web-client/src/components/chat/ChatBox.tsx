49dfc8f69ef9faae63273a5e74c94df097f356b9
Sara Kim <sarakim323@gmail.com>
2023-01-17 20:51:31 -0600

    modified chatbox design

--- web-client/src/components/chat/ChatBox.tsx
+++ web-client/src/components/chat/ChatBox.tsx
@@ -11,7 +11,7 @@ function ChatBox({ matches }) {
   const pic = matches[0].profile_pic;
   const pic2 = matches[1].profile_pic;
   return (
-    <div className="my-10 w-90 h-96 flex flex-col border shadow-md bg-white rounded">
+    <div className="m-10 max-w-sm min-h-fit flex flex-col border shadow-md bg-white rounded-lg bg-white border border-gray-200 shadow-md dark:bg-warmGray-700 dark:border-gray- gap-2 content-center">
       <div className="flex items-center justify-between border-b p-5">
         <div className="flex items-center">
           <img className="rounded-full w-10 h-10" src={pic} />
@@ -21,29 +21,10 @@ function ChatBox({ matches }) {
                 {matches[0].name}
               </a>
             </div>
-            <div className="text-xs text-gray-600">Online</div>
+            <div className="text-xs text-gray-600">{matches[0].breed}</div>
           </div>
         </div>
         <div>
-          <a
-            className="inline-flex hover:bg-indigo-50 rounded-full p-2"
-            href="#"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4"
-              />
-            </svg>
-          </a>
           <button
             className="inline-flex hover:bg-indigo-50 rounded-full p-2"
             type="button"
@@ -72,13 +53,13 @@ function ChatBox({ matches }) {
             {matches[0].name}
           </a>
         </div>
-        <div className="flex-1 bg-indigo-400 text-white p-2 rounded-lg mb-2 relative">
+        <div className="flex-1 bg-indigo-100 text-gray-800 p-2 rounded-lg mb-2 relative">
           <div>Hey! Wassup?</div>
-          <div className="absolute left-0 top-1/2 transform -translate-x-1/2 rotate-45 w-2 h-2 bg-indigo-400"></div>
+          <div className="absolute left-0 top-1/2 transform -translate-x-1/2 rotate-45 w-2 h-2 bg-indigo-100"></div>
         </div>
       </div>
       <div className="flex items-center flex-row-reverse mb-4 p-5">
-        <div className="flex-none flex flex-col items-center space-y-1 mr-4">
+        <div className="flex-none flex flex-col items-center space-y-1 ml-4">
           <img className="rounded-full w-10 h-10" src={pic2} />
           <a href="#" className="block text-xs hover:underline">
             {matches[1].name}
@@ -86,11 +67,10 @@ function ChatBox({ matches }) {
         </div>
         <div className="flex-1 bg-indigo-400 text-white p-2 rounded-lg mb-2 relative">
           <div>Nm - wbu?</div>
-          <div className="absolute right-0 top-1/2 transform -translate-x-1/2 rotate-45 w-2 h-2 bg-indigo-400"></div>
+          <div className="absolute right-0 top-1/2 transform translate-x-1/2 rotate-45 w-2 h-2 bg-indigo-400"></div>
         </div>
       </div>
-      {/* <div className="flex items-center border-t p-2"></div> */}
-      <div className="w-11/12">
+      <div className="flex items-center mb-4 p-4">
         <input
           className="w-full rounded-full border border-gray-200 px-4"
           type="text"
@@ -98,10 +78,8 @@ function ChatBox({ matches }) {
           placeholder="Aa"
           autoFocus
         />
-      </div>
-      <div>
         <button
-          className="inline-flex hover:big-indigo-50 rounded-full p-2"
+          className="inline-flex hover:bg-indigo-50 rounded-full p-2"
           type="button"
         >
           <svg
@@ -115,11 +93,12 @@ function ChatBox({ matches }) {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
             />
           </svg>
         </button>
       </div>
+      <div></div>
     </div>
   );
 }
