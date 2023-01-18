51eb37e57b22eed6d1e92d6782df1679a0d963d7
nathan7432 <steelersfan5052@yahoo.com>
2023-01-18 17:32:05 -0500

    lots of routes

--- api-server/src/db.ts
+++ api-server/src/db.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 mongoose.connect('mongodb://127.0.0.1:27017/fuzzy-friends');
 const UserSchema = new mongoose.Schema(
   {
+    userId: String,
     name: String,
     friends: [String],
   },
@@ -10,7 +11,15 @@ const UserSchema = new mongoose.Schema(
 );
 const User = mongoose.model('User', UserSchema);
 
+const RequestSchema = new mongoose.Schema({
+  userId: String,
+  senderId: String,
+});
+
+const Request = mongoose.model('Request', RequestSchema);
+
 const ProfileSchema = new mongoose.Schema({
+  userId: String,
   name: String,
   age: Number,
   breed: String,
@@ -59,27 +68,26 @@ const messageSchema = new mongoose.Schema(
 const Message = mongoose.model('Message', messageSchema);
 
 export const db = {
-  // addFriend: () => {
-
-  // },
-  // createProfile: () => {
-
-  // },
   addUser: () => {
     const newUser = new User({ name: 'test', friends: ['1', '2'] });
     return newUser.save();
   },
-  getMessages: async (user: string, mate: string) => {
-    const listOfMessages: object[] = [];
-    await Message.find({ senderId: user, receiverId: mate })
-      .then((data) => {
-        listOfMessages.push(data);
-        return Message.find({ senderId: mate, receiverId: user });
-      })
-      .then((data) => {
-        listOfMessages.push(data);
-      });
-    return listOfMessages;
+  getMessages: (senderId: string, receiverId: string) => {
+    let listOfMessages: object[] = [];
+    return new Promise((resolve, reject) => {
+      Message.find({ senderId, receiverId })
+        .then((data) => {
+          listOfMessages = data;
+          return Message.find({ senderId: receiverId, receiverId: senderId });
+        })
+        .then((data) => {
+          listOfMessages = [...listOfMessages, ...data];
+          resolve(listOfMessages);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
   },
   addMessage: (
     senderId: string,
@@ -90,6 +98,38 @@ export const db = {
       const newMessage = new Message({ senderId, receiverId, messageContent });
       newMessage
         .save()
+        .then(() => {
+          resolve('Success');
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
+  getRequests: (userId: string) => {
+    return Request.find({ userId });
+  },
+  addRequest: (userId: string, receiverId: string) => {
+    const newRequest = new Request({ userId: receiverId, senderId: userId });
+    return newRequest.save();
+  },
+  getFriends: (userId: string) => {
+    return User.findOne({ userId: userId }).then((data) => {
+      return Profile.find({
+        userId: { $in: data.friends },
+      });
+    });
+  },
+  addFriend: (userId: string, senderId: string) => {
+    return new Promise((resolve, reject) => {
+      Request.findOneAndRemove({ userId, senderId })
+        .then(() => {
+          return User.findOne({ userId });
+        })
+        .then((user) => {
+          user.friends.push(senderId);
+          return user.save();
+        })
         .then(() => {
           resolve('Success');
         })
--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -5,6 +5,7 @@ const port = 3000;
 import { discover, friends, messages, sampleEvents } from './mock_data';
 import { Request, Response } from 'express';
 import { db } from './db';
+import { request } from 'axios';
 
 app.use(express.json());
 app.use(cors());
@@ -19,23 +20,56 @@ app.post('/users/:id/messages/:mateId', (req: Request, res: Response) => {
     .catch((err) => res.send(err));
 });
 
-app.get('/user/:id/messages/:mateId', (req: Request, res: Response) => {
-  // res.send(messages);
-  try {
-    const messages = db.getMessages(req.params.id, req.params.mateId);
-    res.send(messages);
-  } catch (err) {
-    res.sendStatus(401);
-  }
+app.get('/users/:id/messages/:mateId', (req: Request, res: Response) => {
+  db.getMessages(req.params.id, req.params.mateId)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
-app.get('/users/:id/friends', (req: Request, res: Response) =>
-  res.send(friends)
-);
-
-app.put('/users/:id/friends/*', (req: Request, res: Response) => {
-  res.sendStatus(200);
+app.get('/users/:id/friends', (req: Request, res: Response) => {
+  db.getFriends(req.params.id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
+
+app.post('/users/:id/friends/:mateId', (req: Request, res: Response) => {
+  db.addFriend(req.params.id, req.params.mateId)
+    .then(() => res.sendStatus(201))
+    .catch((err) => res.send(err));
+});
+
+app.get('/users/:id/requests', (req: Request, res: Response) => {
+  db.getRequests(req.params.id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post('/users/:id/requests/:mateId', (req: Request, res: Response) => {
+  db.addRequest(req.params.id, req.params.mateId)
+    .then(() => {
+      res.send(201);
+    })
+    .catch(() => {
+      res.send(501);
+    });
+});
+
+// app.put('/users/:id/friends/*', (req: Request, res: Response) => {
+//   db.
+//   res.sendStatus(200);
+// });
 
 app.get('/users/:id/discover', (req: Request, res: Response) => {
   res.send(discover);
