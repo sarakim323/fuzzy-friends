2485491840216f4c6329be9528f122186cd7a779
Linh <linhscholar71@gmail.com>
2023-01-19 10:46:55 -0500

    add prop-types

--- web-client/src/pages/Profile.tsx
+++ web-client/src/pages/Profile.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Description from '../components/profile/Description';
 import EditForm from '../components/profile/EditForm';
 import CarouselCard from '../components/profile/Carousel';
