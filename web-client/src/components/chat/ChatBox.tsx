8b83d5ca5cf95e5757da9ea470b4656d4cab568a
Sara Kim <sarakim323@gmail.com>
2023-01-19 12:26:52 -0600

    enabled chat input box and added send msg func

--- web-client/src/components/chat/ChatBox.tsx
+++ web-client/src/components/chat/ChatBox.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChatInput from './ChatInput';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 interface ChatBoxProps {
   mate: Mate;
@@ -8,10 +9,21 @@ interface ChatBoxProps {
 }
 
 const ChatBox: React.FC<ChatBoxProps> = ({ currentChat, mate, user }) => {
-  // const [messages, setMessages] = useState([]);
-  // const scrollRef = useRef();
-  // const [arrivalMessage, setArrivalMessage] = useState(null);
-  // console.log('currentChat: ', currentChat); // passing
+  const [newMessage, setnewMessage] = useState('');
+
+  const sendMessage = async () => {
+    console.log('message sent');
+    if (newMessage !== '') {
+      const messageData = {
+        senderId: '',
+        receiverId: '',
+        messageContent: newMessage,
+      };
+      // POST message request
+      setnewMessage('');
+    }
+  };
+
   return (
     <div className="m-10 max-w-sm min-h-fit flex flex-col border shadow-md bg-white rounded-lg bg-white border border-gray-200 shadow-md dark:bg-warmGray-700 dark:border-gray- gap-2 content-center">
       <div className="flex items-center justify-between border-b p-5">
@@ -26,45 +38,35 @@ const ChatBox: React.FC<ChatBoxProps> = ({ currentChat, mate, user }) => {
             <div className="text-xs text-gray-600">{mate.breed}</div>
           </div>
         </div>
-        <div>
-          <button
-            className="inline-flex hover:bg-indigo-50 rounded-full p-2"
-            type="button"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
       </div>
-      {currentChat.map((message) => {
-        return (
-          <ChatInput
-            key={message.userId}
-            message={message}
-            mate={mate}
-            user={user}
-          />
-        );
-      })}
-      <div className="flex items-center mb-4 p-4">
+      <ScrollToBottom>
+        {currentChat.map((message) => {
+          return (
+            <ChatInput
+              key={message.userId}
+              message={message}
+              mate={mate}
+              user={user}
+            />
+          );
+        })}
+      </ScrollToBottom>
+      <div className="flex items-center mb-4 p-4 w-full">
         <form className="w-full rounded-full border border-gray-200 px-4">
-          <input type="text" value="" placeholder="Aa" autoFocus />
+          <input
+            type="text"
+            value={newMessage}
+            onChange={(event) => {
+              setnewMessage(event.target.value);
+            }}
+            placeholder="Aa"
+            autoFocus
+          />
         </form>
         <button
-          className="inline-flex hover:bg-indigo-50 rounded-full ml-1 p-2"
+          className="inline-flex hover:bg-indigo-50 rounded-full p-2"
           type="button"
+          onClick={sendMessage}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +79,7 @@ const ChatBox: React.FC<ChatBoxProps> = ({ currentChat, mate, user }) => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
             />
           </svg>
         </button>
